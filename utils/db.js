1535d5bff85eb9ba94a6eecec3f0b3dc9dd24e32
5237-mests <msfnmulgeta@gmail.com>
2023-03-01 11:52:01 +0300

    f

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,12 @@
+import { config } from 'dotenv';
+
+config();
+
+const { MongoClient } = require("mongodb");
+
+const uri = 'mongodb://127.0.0.1:27017';
+
+const client = new MongoClient(uri);
 
 
 class DBClient {
