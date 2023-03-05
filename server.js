aac645163bfe284a88cc08de31c809924f61d2d0
GetacherD <getacher.d@gmail.com>
2023-03-05 13:04:39 +0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dbClient from './utils/db';
+import redisClient from './utils/redis';
+// import dbClient from './utils/db';
 
 const app = express();
 
@@ -7,7 +10,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('', require('./routes/index'));
+// if (!redisClient.isAlive() && !dbClient.isAlive()) {
+//   console.log("Connecting to server mongodb and redis")
+// }
+// else if (redisClient.isAlive() && !dbClient.isAlive()) {
+//   console.log("Connecting to server mongodb")
+// } else if (dbClient.isAlive() && !redisClient.isAlive()) {
+//   console.log("Connecting to server redis")
+// }
+// else {
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// }
+// while (!redisClient.isAlive() || !dbClient.isAlive()) {
+//   console.log("Conecting")
+
+// }
+function connectDb() {
+  console.log('Try Connection');
+  if (dbClient.isAlive() && redisClient.isAlive()) {
+    console.log('Connected to mongo');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } else {
+    setTimeout(() => connectDb(), 1000);
+  }
+}
+connectDb();
