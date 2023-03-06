8b5070efd66a5e94612e50970682a05108029f71
GetacherD <getacher.d@gmail.com>
2023-03-06 09:54:49 +0300

    fix basic auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -8,7 +8,8 @@ export default class AuthController {
   static async getConnect(req, res) {
     //     console.log(req);
     const auth = req.header('Authorization');
-    if (auth.slice(0, 5).toLowerCase() !== 'Basic'.toLowerCase()) {
+    console.log(auth.slice(0, 7))
+    if (auth.slice(0, 6).toLowerCase() !== 'Basic '.toLowerCase()) {
       res.status(401).json({ error: 'Unauthorized' });
       return;
     }
