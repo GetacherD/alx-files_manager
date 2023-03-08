56a6a5659da475b187d2b0e1e6537db8d92ae7ac
5237-mests <msfnmulgeta@gmail.com>
2023-03-08 22:06:01 +0300

    d

--- worker.js
+++ worker.js
@@ -1,12 +1,36 @@
-import Queue from "bull/lib/queue";
+import { writeFileAsync } from 'fs'
+import Queue from 'bull/lib/queue';
+import dbClient from './utils/db';
+import {ObjectId} from 'mongodb'
+const imageThumbnail = require('image-thumbnail')
 
-const fileQueue = new Queue('file queue');
+const fileQueue = new Queue('fileQueue');
+
+async function generateThumbnail(filepath, size) {
+  const thumb = await imageThumbnail(filepath, {width: size});
+  return writeFileAsync(`${filepath}_${size}`, thumb);
+}
 
 fileQueue.process(async (job, done) => {
-  console.log(job)
-  
+  const userId = job.data.userId || null;
+  const fileId = job.data.fileId || null;
 
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  if (!fileId) {
+    throw new Error('Missing userId');
+  }
+
+  const file = await (await dbClient.filesCollection()).findOne({userId:ObjectId(userId), _id: ObjectId(fileId)});
+
+  if (!file) {
+    throw new Error('File not found');
+  }
+
+  const width = [500, 250, 100];
+  Promise.all(width.map((size) => generateThumbnail(file.localPath, size)))
+   .then( () => {
+      done();
+    });
 })
-
-
-module.exports = fileQueue;
